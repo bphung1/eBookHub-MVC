74afc685c3f8bfaf25f737ed6c6e687acf00c1ca
Bao Phung <b.giaphung@gmail.com>
2020-11-21 21:54:32 -0600

    started adding context to Mapper class

--- eBookHubApp/eBookHub.DataAccess/Mapper.cs
+++ eBookHubApp/eBookHub.DataAccess/Mapper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eBookHub.Library.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,27 @@ namespace eBookHub.DataAccess
 {
     public static class Mapper
     {
+        public static Account Map(Entities.Account account) => new Account
+        {
+            LoginID = account.LoginId,
+            DisplayName = account.DisplayName,
+            Username = account.DisplayName,
+            Email = account.EmailAddress,
+            AboutMe = account.AboutMe,
+            Admin = (bool)account.Administrator,
+            UpcomingBook = account.UpcomingBooks
+        };
+
+        public static Entities.Account Map(Account account) => new Entities.Account
+        {
+            LoginId = account.LoginID,
+            DisplayName = account.DisplayName,
+            UserName = account.Username,
+            EmailAddress = account.Email,
+            AboutMe = account.AboutMe,
+            Administrator = account.Admin,
+            UpcomingBooks = account.UpcomingBook
+        };
+
     }
 }
